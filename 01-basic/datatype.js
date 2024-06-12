4f61fcb0d155bba9c0f70248ccb1638534569ffe
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-06-12 11:23:42 +0000

    code

---
+++ 01-basic/datatype.js
@@ -0,0 +1,11 @@
+console.log(typeof "shushil");
+console.log(3+3);
+//number => 2 to power 53
+//brgint
+//sting => ""
+//Boolean => true /false
+//null => Standalone value  null ek object type haie
+//undefined => undefined ek undefined haie 
+//symbol => unique haie 
+
+console.log(typeof null)
--- 01-basic/main.js
+++ 01-basic/main.js
@@ -1,1 +1,13 @@
-console.log("shushil")
+const accountId = 122544
+let accountEmail = "connectwithshushil@gmail.com"
+var accountpassword = "26523546"
+accountCity = "Nepal"
+let accountstate = 
+
+
+console.log([accountId, accountEmail, accountpassword, accountCity])
+
+/*
+prefer  not to use var
+because of issue in block scope and functional scope 
+*/
