686101c426726dabf388820ba88d4e9f49cf9da1
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-06-24 12:06:06 +0000

    conversion

--- 01-basic/conversionoperation.js
+++ 01-basic/conversionoperation.js
@@ -2,3 +2,17 @@ let score = 33
 
 console.log(typeof score );
 // console.log(typeof (score));
+
+let isLoggedIn = 33
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// true => 33
+// false => 0
+// shushil => true
+
+let someNumber = "20"
+let stringNumber =  String (someNumber)
+console.log(someNumber);
+console.log(typeof someNumber); 
