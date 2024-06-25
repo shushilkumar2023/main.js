84f0f6e18f36dde2714318cb2b1f7ee3a5b3bf8e
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-06-25 06:08:45 +0000

    conversion

--- 01-basic/conversionoperation.js
+++ 01-basic/conversionoperation.js
@@ -20,7 +20,19 @@ console.log(typeof someNumber);
 
 // operation
 
-let str1 = hello
-let str2 = Shushil
+let str1 = "hello"
+let str2 = "Shushil"
 let str3 = str1 + str2
 console.log(str3);
+
+console.log(1+1+"2");
+console.log("1"+1+22+4);
+console.log((2+2)*5/5-5);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
