3d175249860aace371b0fb2dbbcf8e69fb9cff6b
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-06-26 10:56:21 +0000

    comparsion

---
+++ 01-basic/comparison.js
@@ -0,0 +1,10 @@
+// postfix increment    postfix Decrement
+// X++                  x--
+
+// prefix increment     Prefix Decrement
+// ++x                     --x
+
+let x = 5
+y = x++
+console.log(x);
+console.log(y);
