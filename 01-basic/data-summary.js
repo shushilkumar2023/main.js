20228ccc68916dc48a8de8b56dc1c3ef9f17d7fb
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-04 16:13:43 +0000

    datasummary

---
+++ 01-basic/data-summary.js
@@ -0,0 +1,41 @@
+
+// Primitive datatypes
+// 1 String
+// 2 Number
+// 3 Boolean
+// 4 Symbol
+// 5 null
+// 6 undefined
+// 7 BigInt
+
+// //  non - Primitives datatypes (References)
+// 1 Array
+// 2 Object
+// 3 Function
+
+// const score = 100 
+// const scorevalue = 100.3 
+// const isLoggedIN = false 
+// const outsideTemp = null
+// let userEmail;
+
+// const Id = symbol('123')
+// const anotherId = symbol('123')
+// console.log(Id === anotherId);
+
+
+// There are two types of memory storage
+
+// 1 stack and 
+// 2 Heap 
+
+// Stack primitives data 
+// Heap non Primitives data  
+
+// for example  Stack memory 
+
+let myname = "shushil kumar yadav"
+let anothername = myname
+anothername = "innocent"
+console.log(myname);
+console.log(anothername);
