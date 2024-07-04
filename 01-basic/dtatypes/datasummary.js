c1aa15f836fb93f62ccff87dfbbd1bc86361087f
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-04 06:57:25 +0000

    datasummary

---
+++ 01-basic/dtatypes/datasummary.js
@@ -0,0 +1,24 @@
+//  primitives datatyes 
+//  1 String 
+//  2 Number 
+//  3 Boolean
+//  4 null
+// 5 undefined
+// 6 Symbol
+// 7 BigInt
+
+// # Non - Primitives datatypes (Reference ) 
+//  1 Arrya 
+//  2 Object
+//  3 Functions
+
+const score = 100
+const scorevalue = 100.3
+const islooggedIn = false 
+const OutsideTem = null
+let userEmail;
+
+
+
+
+
