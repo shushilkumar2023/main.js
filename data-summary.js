b2aee5ae87860a2278958fe4adf5af4e420debe6
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-04 07:22:54 +0000

    data

---
+++ data-summary.js
@@ -0,0 +1,26 @@
+// Primitive datatypes
+// 1 String
+// 2 Number
+// 3 Boolean
+// 4 Symbol
+// 5 null
+// 6 undefined
+// 7 BigInt
+
+// //  non - Primitives datatypes (References)
+// 1 Array
+// 2 Object
+// 3 Function
+
+// const score = 100 
+// const scorevalue = 100.3 
+// const isLoggedIN = false 
+// const outsideTemp = null
+// let userEmail;
+
+const Id = symbol('123')
+const anotherId = symbol('123')
+console.log(Id === anotherId);
+
+
+
