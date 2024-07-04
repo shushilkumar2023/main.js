585f3f33e002ba902a09830c8fd0b0235bc592d1
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-04 06:18:56 +0000

    comparision

--- 01-basic/comparision.js
+++ 01-basic/comparision.js
@@ -9,7 +9,26 @@
 // console.log(x);
 // console.log(y);
 
-let y,x = 6
-y = ++x;
-console.log(x);
-console.log(y);
+// let y,x = 6
+// y = ++x;
+// console.log(x);
+// console.log(y);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// The reason is that an equality check == and comparision ><=>=null to a numbr tresting it as 0 That's why (3) null >=0 is true (1) null > 0 is false 
+
+// Example 
+// > - Greater than 
+// < Less than
+// >= true 
+// <= false 
+
+console.log(null <= 0); output = True
+console.log(null >= 0); output = True  
+console.log(null == 0); output = false 
+console.log(null > 0); output = false
+console.log(null < 0); output = false
+// console.log("2" === 2);
