6662b77f108c830f7c94eedd89f3272fb5c4a2cb
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-07 12:19:25 +0000

    dateandtime

---
+++ 01-basic/dateandtime.js
@@ -0,0 +1,9 @@
+let myDate = new Date ()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.getTimezoneOffset());
