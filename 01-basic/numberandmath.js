de991484a656c1ff89ab56f2a6f51330975a4234
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-07 12:03:10 +0000

    numberandmath

--- 01-basic/numberandmath.js
+++ 01-basic/numberandmath.js
@@ -1,14 +1,31 @@
-// console.log(typeof(typeof "a"));
+// // console.log(typeof(typeof "a"));
+                                    //    Number code 
 
-const score = 400
-const balance = new Number(200)
-console.log(balance);
-console.log(typeof "balance");
-console.log(balance.toFixed(5));
-console.log(balance.toString().length);
+// const score = 400
+// const balance = new Number(200)
+// console.log(balance);
+// console.log(typeof "balance");
+// console.log(balance.toFixed(5));
+// console.log(balance.toString().length);
 
-const anotherNumber = 2454.25
-console.log(anotherNumber.toPrecision(2));
+// const anotherNumber = 2454.25
+// console.log(anotherNumber.toPrecision(2));
 
-const hundred = 10000000
-console.log(hundred.toLocaleString());
+// const hundred = 10000000
+// console.log(hundred.toLocaleString());
+
+//                                                math code 
+
+// console.log(Math);
+// console.log(Math.abs);
+// console.log(Math.ceil(4,5));
+// console.log(Math.floor(4,5));
+// console.log(Math.round(4,5));
+// console.log(Math.max(4,3,5,8));
+// console.log(Math.min(4,3,5,8));
+//  console.log(Math.random()*10 + 1);
+
+const min = 10
+const Max = 20
+
+console.log(Math.floor( Math.random() * (Max - min + 1)) + min);
