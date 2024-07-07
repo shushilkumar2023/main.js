25db607a06973ef214cd4cfae86ef06b28ee06aa
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-07 09:09:34 +0000

    numberandmath

---
+++ 01-basic/numberandmath.js
@@ -0,0 +1,14 @@
+// console.log(typeof(typeof "a"));
+
+const score = 400
+const balance = new Number(200)
+console.log(balance);
+console.log(typeof "balance");
+console.log(balance.toFixed(5));
+console.log(balance.toString().length);
+
+const anotherNumber = 2454.25
+console.log(anotherNumber.toPrecision(2));
+
+const hundred = 10000000
+console.log(hundred.toLocaleString());
