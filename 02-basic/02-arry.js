4c24fc7bec00efd9f27829842e9d11dfee906978
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-08 16:52:39 +0000

    arry

--- 02-basic/02-arry.js
+++ 02-basic/02-arry.js
@@ -1,9 +1,9 @@
-const marval_heroes = ["thor","ironman","spiderman","batman"]
-const person = ["bibek","prabesh","suraj"]
 // console.log(marval_heroes);
 // marval_heroes.push(person)
 // console.log(marval_heroes);
 // console.log(marval_heroes[3][1]);
+const marval_heroes = ["thor","ironman","spiderman","batman"];
+const person = ["bibek","prabesh","suraj"];
 
-const allmarval_heroes = marval_heroes.concat(person)
-console.log(allmarval_heroes);
+const allMarval_heroes = marval_heroes.concat(person);
+console.log(allMarval_heroes);
