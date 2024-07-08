a62d7d3ae0d2e28964b44c83e3dcc36674d33cc0
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-08 17:17:29 +0000

    array

---
+++ 02-basic/02-array.js
@@ -0,0 +1,22 @@
+// console.log(marval_heroes);
+// marval_heroes.push(person)
+// console.log(marval_heroes);
+// console.log(marval_heroes[3][1]);
+// const marval_heroes = ["thor", "ironman", "spiderman", "batman"];
+// const person = ["bibek", "prabesh", "suraj"];
+
+// const allMarval = marval_heroes.concat(person);
+// console.log(allMarval);
+
+// const all_New_heroes = [...marval_heroes, ...allMarval]
+// console.log(all_New_heroes);
+
+const another_arry = [1,2,4,5,[1,2,3,4], 4, [1,5,4,7]]
+// console.log(another_arry);
+
+const real_another_arry = another_arry.flat(Infinity)
+console.log(real_another_arry);
+
+console.log(Array.isArray("shushil"))
+ console.log(Array.from("shushil"))
+
---
+++ 02-basic/array.js
@@ -0,0 +1,38 @@
+                            // Arry 
+ const myArry = [0,1,2,3,4,5]
+const heroes = ["thor","spiderman","batman","ironman"]
+// const myArry2 =  new Array(1,5,8,7,5)
+//  console.log(myArry,heroes);
+// console.log(myArry2); 
+// console.log(myArry2[1]);
+
+
+                                // Arry methods
+//  myArry.push(6)
+// // console.log(myArry); The new length of the array is returned when the method call completes. If you wanted to store the new array length in a variable, you could do something like this:
+//  myArry.pop()  To remove the last item from the array, use pop().
+//  console.log(myArry); 
+
+//  myArry.unshift(4)  To add an item to the start of the array, use unshift(): 
+//  console.log(myArry)
+
+// myArry.shift(6)  To remove the first item from an array, use shift():
+// console.log(myArry);
+
+// console.log(myArry.includes(6));
+// console.log(myArry.indexOf(3));
+
+// const newArry = myArry.join()
+// console.log( typeof newArry);
+// console.log(myArry);
+
+//  
+console.log("A",myArry);
+const myn1 = myArry.slice(1,3)
+ console.log(myn1);
+ console.log("B",myArry);
+
+const myn2 = myArry.splice(1,3)
+ console.log(myn2);
+
+ console.log("c",myArry);
