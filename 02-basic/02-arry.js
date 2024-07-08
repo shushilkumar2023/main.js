4f20ef411efa170b89517aa181872bfc2454110a
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-08 16:48:49 +0000

    arrytwo

--- 02-basic/02-arry.js
+++ 02-basic/02-arry.js
@@ -1,2 +1,9 @@
 const marval_heroes = ["thor","ironman","spiderman","batman"]
-console.log(marval_heroes);
+const person = ["bibek","prabesh","suraj"]
+// console.log(marval_heroes);
+// marval_heroes.push(person)
+// console.log(marval_heroes);
+// console.log(marval_heroes[3][1]);
+
+const allmarval_heroes = marval_heroes.concat(person)
+console.log(allmarval_heroes);
