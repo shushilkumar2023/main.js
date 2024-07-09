6b28da464deb82d3ae20c604d26f4e3bb3838df8
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-09 12:21:30 +0000

    object

--- 02-basic/03-objects.js
+++ 02-basic/03-objects.js
@@ -1,23 +1,36 @@
-const mySmb1 = Symbol(14)
-const myObj = {
-    name:"shushil kumar yadav",
-    age:21,
-    mySmb1:"mySmb2",  // symbol 
-    location:"sanepa",
-    number:142545651,
-    email:"connectwithshushil@gamil.com",
-    isLoggedIn:false,
-    lastWorkdays:["sunday","monday"]
-}
+// //singleton
+// //object literals
+//  Object.create // Objects constructor provides funcitiality common to all javascripts objects  
+//  const mySmb1 = Symbol("key1")
+// const myObj = {
+//     name:"shushil kumar yadav",
+//     age:21,
+//     [mySmb1]:"mySmb2",  // symbol 
+//     location:"sanepa",
+//     number:142545651,
+//     email:"connectwithshushil@gamil.com",
+//     isLoggedIn:false,
+//     lastWorkdays:["sunday","monday"]
+// }
 
-console.log(myObj);
-// console.log(myObj["name"]);
-
-// console.log(myObj["name"]);
- console.log(typeof mySmb1);
-// myObj.email= "shushil@gmail.com"
-// Object.freeze(myObj)
-//  myObj.email = "shuhsil@123"
 // console.log(myObj);
+// console.log(myObj["email"]);
+// console.log(myObj["name"]);
 
+// // console.log(myObj["name"]);
+//   console.log(typeof mySmb1);
+// // myObj.email= "shushil@gmail.com"
+// // myObj.email = "shuhsil@123"
+// // Object.freeze(myObj)
+// // console.log(myObj);
+
+
+// myObj.greeting = function (params) {
+//     console.log("hello myobject");
+// }
+// myObj.greeting2 = function (params) {
+//     console.log(`hello myobject, ${this.name}`);
+// }
+// console.log(myObj.greeting());
+// console.log(myObj.greeting2());
 
