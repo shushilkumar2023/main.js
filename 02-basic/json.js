8ef84394ebca07ad2de9424f5a4bdca70f8ed983
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-09 16:14:12 +0000

    json

---
+++ 02-basic/json.js
@@ -0,0 +1,13 @@
+const course = {
+    book:"js books",
+    price:524,
+    teacher:"bibek"
+}
+
+// console.log(teacher);
+ 
+const {teacher} = course
+
+console.log(teacher);
+console.log(typeof teacher);
+
