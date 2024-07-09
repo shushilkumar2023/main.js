62b6b88b208a22694e7650714b5ca8daa728abac
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-09 07:09:04 +0000

    array

--- 02-basic/02-array.js
+++ 02-basic/02-array.js
@@ -11,12 +11,18 @@
 // const all_New_heroes = [...marval_heroes, ...allMarval]
 // console.log(all_New_heroes);
 
-const another_arry = [1,2,4,5,[1,2,3,4], 4, [1,5,4,7]]
-// console.log(another_arry);
+// const another_arry = [1,2,4,5,[1,2,3,4], 4, [1,5,4,7]]
+// // console.log(another_arry);
 
-const real_another_arry = another_arry.flat(Infinity)
-console.log(real_another_arry);
+// const real_another_arry = another_arry.flat(Infinity)
+// console.log(real_another_arry);
 
-console.log(Array.isArray("shushil"))
- console.log(Array.from("shushil"))
+// console.log(Array.isArray("shushil"))
+//  console.log(Array.from("shushil"))
+
+let score1 = 100
+let score2 = 100
+let  score3 = 100
+// console.log(Array.of(score1,score2,score3));
+console.log(Array.from(score1,score2,score3));
 
