b87e241eca6ae7dd1e3e92dbd5bc1d70f60465a4
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-09 07:08:07 +0000

    objects

---
+++ 02-basic/03-objects.js
@@ -0,0 +1,23 @@
+const mySmb1 = Symbol(14)
+const myObj = {
+    name:"shushil kumar yadav",
+    age:21,
+    mySmb1:"mySmb2",  // symbol 
+    location:"sanepa",
+    number:142545651,
+    email:"connectwithshushil@gamil.com",
+    isLoggedIn:false,
+    lastWorkdays:["sunday","monday"]
+}
+
+console.log(myObj);
+// console.log(myObj["name"]);
+
+// console.log(myObj["name"]);
+ console.log(typeof mySmb1);
+// myObj.email= "shushil@gmail.com"
+// Object.freeze(myObj)
+//  myObj.email = "shuhsil@123"
+// console.log(myObj);
+
+
