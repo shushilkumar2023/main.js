91d90bc965b6f7bfaa34c776e20125dceed9ceba
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-10 06:28:56 +0000

    functionandparameter

---
+++ 03-basic/functionandparameter.js
@@ -0,0 +1,30 @@
+//    const personname = {
+    //     name:"shushil",
+//     //     age:25
+//     function person () {
+//         console.log("s");
+//         console.log("h");
+//         console.log("u");
+//         console.log("s");
+//         console.log("h");
+//         console.log("i");
+//         console.log("l");
+//  }
+// person()
+
+// function anotherperson (number1, number2) { //passing and two Aggrement 
+//     console.log(number1 + number2);
+// }
+//  anotherperson(3,7)  // this code is  output Add
+//  anotherperson(3,"5")  
+//  anotherperson(3,null)
+//  anotherperson()
+
+
+function anothername(number1,number2){
+    let result = number1 + number2
+    return result
+}
+
+const result = anothername(3, 8)
+console.log("Result:", result);
