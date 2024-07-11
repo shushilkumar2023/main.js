0c9448c6a5315a28cb6dbecfa047109c2d53264a
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-11 06:01:36 +0000

    globalandlocalscope

---
+++ 03-basic/globalandlocalscope.js
@@ -0,0 +1,21 @@
+
+let a = 200
+
+if (true){
+    let a = 20
+    const b = 40
+    console.log("Inner:", a);
+ 
+    function addnum () {
+        
+    }
+}
+
+
+// function sum (a,b,c) {
+//     let totalsum = a+b+c
+// }
+
+console.log(a);
+// console.log(b);
+// console.log(c);
