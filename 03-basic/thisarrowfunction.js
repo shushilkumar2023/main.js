03f73d580d2d3c449d19f02bf7012d7b05ef9be8
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-11 12:31:42 +0000

    thisarrowfunction

---
+++ 03-basic/thisarrowfunction.js
@@ -0,0 +1,25 @@
+// const user = {
+//     username:"bibek",
+//     order: 254,
+//     welcomemessage: function () {
+//         console.log(`${this.username}, welcome to my new website`);
+//         console.log(`${this.order}, welcome to my new website`);
+//         console.log(this);
+//     }
+// }
+
+// user.welcomemessage()
+// user.username = "shuhsil"
+// user.order = 254
+// user.welcomemessage()
+
+
+// const addtwo = (num1, num2) => { //single 
+//     return num1 +num2
+// }
+
+// const addtwo = (num1, num2) => (num1 + num2)
+const addtwo = (num1, num2) => ({username "shuhsil"})
+
+
+console.log(addtwo(3, 4));
