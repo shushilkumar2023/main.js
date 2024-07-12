d5c6a1e95766aa2397b537b7ec3eb82a628d5371
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-12 05:39:05 +0000

    iife

---
+++ 03-basic/iife.js
@@ -0,0 +1,14 @@
+(function person () {
+    console.log(`my name is shushil`);
+})();
+
+function person () {
+    console.log(`my name is shushil`);
+}
+
+ person();
+
+ (() => {
+    console.log(`my name is shushil`);
+ }) ()
+
