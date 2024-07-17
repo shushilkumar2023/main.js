275c2d80099958a3b91ca23f99ccbe7b872f4b12
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-17 07:36:08 +0000

    controlflow

---
+++ 04-basic/controlflow.js
@@ -0,0 +1,22 @@
+//  const score = 200
+
+//  if (score > 100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+//  }
+
+
+const ATMcard = 2
+
+if (ATMcard > 5) {
+    console.log("your Atmcard expired on 5 days ");
+}else if (ATMcard > 4) {
+    console.log("your Atmcard expired on 4 days");
+}
+else if (ATMcard > 3) {
+    console.log("your Atmcard expired on 3 days");
+}else if (ATMcard > 2) {
+    console.log("your Atmcard expired on 2 days");
+}else {
+    console.log("you Atmcard on expired ");
+}
