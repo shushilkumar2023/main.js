06e50fbfdf8a98b9d90095bfa782131059d73c0a
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-17 14:47:35 +0000

    controlflow

--- 04-basic/controlflow.js
+++ 04-basic/controlflow.js
@@ -6,17 +6,32 @@
 //  }
 
 
-const ATMcard = 2
+// const ATMcard = 2
 
-if (ATMcard > 5) {
-    console.log("your Atmcard expired on 5 days ");
-}else if (ATMcard > 4) {
-    console.log("your Atmcard expired on 4 days");
+// if (ATMcard > 5) {
+//     console.log("your Atmcard expired on 5 days ");
+// }else if (ATMcard > 4) {
+//     console.log("your Atmcard expired on 4 days");
+// }
+// else if (ATMcard > 3) {
+//     console.log("your Atmcard expired on 3 days");
+// }else if (ATMcard > 2) {
+//     console.log("your Atmcard expired on 2 days");
+// }else {
+//     console.log("you Atmcard on expired ");
+// }
+
+const userLoggedIn = true 
+const debitCard = true
+const usergoogle = true
+const loggedInfronEmail = true 
+
+if (userLoggedIn && debitCard && usergoogle) {
+    console.log("Allow to buy  this course");
+} else{
+    console.log("don't buy this course" );
 }
-else if (ATMcard > 3) {
-    console.log("your Atmcard expired on 3 days");
-}else if (ATMcard > 2) {
-    console.log("your Atmcard expired on 2 days");
-}else {
-    console.log("you Atmcard on expired ");
+
+if (loggedInfronEmail || usergoogle ) {
+    console.log("user logged in ");
 }
