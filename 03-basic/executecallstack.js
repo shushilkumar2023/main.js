00eb2501fc7ac130e73fdcff916813d9b8b91bf4
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-17 07:01:38 +0000

    callstack

--- 03-basic/executecallstack.js
+++ 03-basic/executecallstack.js
@@ -1,12 +1,33 @@
-let val1 = 40
-let val2 = 50
+// let val1 = 40
+// let val2 = 50
 
-function aadnum(num1 , num2) {
-    let total = (num1+ num2)
-    return total
+// function aadnum(num1 , num2) {
+//     let total = (num1+ num2)
+//     return total
+// }
+
+//  let total1 = (val1 , val2);
+// let total2 = (10 , 20);
+
+// // console.log(("val1 val2"));
+
+//  execute coode and call stack 
+
+function one() {
+    console.log("one");
+    two()
 }
 
- let total1 = (val1 , val2);
-let total2 = (10 , 20);
+function two() {
+    console.log("two");
+    three()
+}
 
-// console.log(("val1 val2"));
+function three() {
+    console.log("three");
+    
+}
+
+one()
+two()
+three()
