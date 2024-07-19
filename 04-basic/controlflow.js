2899f4e0dbca99ad197d826e886296e54c4bd7ed
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-19 07:18:51 +0000

    control

--- 04-basic/controlflow.js
+++ 04-basic/controlflow.js
@@ -16,22 +16,38 @@
 // else if (ATMcard > 3) {
 //     console.log("your Atmcard expired on 3 days");
 // }else if (ATMcard > 2) {
-//     console.log("your Atmcard expired on 2 days");
-// }else {
-//     console.log("you Atmcard on expired ");
+// //     console.log("your Atmcard expired on 2 days");
+// // }else {
+// //     console.log("you Atmcard on expired ");
+// // }
+
+// const userLoggedIn = true 
+// const debitCard = true
+// const usergoogle = true
+// const loggedInfronEmail = false
+
+// if (userLoggedIn && debitCard && usergoogle) {
+//     console.log("Allow to buy  this course");
+// } else{
+//     console.log("don't buy this course" );
 // }
 
-const userLoggedIn = true 
-const debitCard = true
-const usergoogle = true
-const loggedInfronEmail = true 
+// if (loggedInfronEmail || usergoogle ) {
+//     console.log("user logged in ");
+// }
 
-if (userLoggedIn && debitCard && usergoogle) {
-    console.log("Allow to buy  this course");
-} else{
-    console.log("don't buy this course" );
+
+//truthy values 
+// "0", 'false' "", [], {}, function(){}
+
+const userEmail = "connectwithshushil"
+if (userEmail) {
+    console.log("get user email");
+}else {
+    console.log("DOn't have user email ");
 }
 
-if (loggedInfronEmail || usergoogle ) {
-    console.log("user logged in ");
+if (userEmail.length === 0) {
+    console.log("Array is Empty");
+    
 }
