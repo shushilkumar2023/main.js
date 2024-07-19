140dd8f25460493ff5af0b10d928c5f03f9ea420
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-19 07:19:02 +0000

    switch

--- 04-basic/switch.js
+++ 04-basic/switch.js
@@ -1,8 +1,27 @@
-switch (  ) {
-    case value:
-        
+// const month = "jan"
+
+// switch (month) {
+//     case "jan":
+//         console.log("jan");
+//         break;
+
+//         case "feb":
+//             console.log("feb");
+//             break;
+//     default:
+//         break;
+// }
+
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("jan");
         break;
 
+        case 2:
+            console.log("feb");
+            break;
     default:
         break;
 }
