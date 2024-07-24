b712725a445b40f59eda8858a886be11afed77b3
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-24 05:54:50 +0000

    loopadvance

--- 05-iterations/loopadvance.js
+++ 05-iterations/loopadvance.js
@@ -7,11 +7,26 @@
 //     console.log(element);  
 // }
 
-for (let index = 0; index <= 10; index++) {
-  // console.log(`outer loop value: ${index}`);
- for (let j = 0; j <= 10; j++) {
-  // console.log(`inner liner ${j} outer liner ${index}`);
-  console.log(index + "*" + j + '='+ index*j);
- }
+// for (let index = 0; index <= 10; index++) {
+//   // console.log(`outer loop value: ${index}`);
+//  for (let j = 0; j <= 10; j++) {
+//   // console.log(`inner liner ${j} outer liner ${index}`);
+//   console.log(index + "*" + j + '='+ index*j);
+//  }
   
-}
+// }
+
+// break and contiune
+
+// for (let index = 0; index <=20; index++) {
+//   if (index == 5) {
+//     console.log(`dedected is 5`);
+//   }
+// if (index == 7) {
+//   console.log(`your data is save`);
+//   break
+//   continue
+// }
+
+//  console.log(`value of i ${index}`);
+// }
