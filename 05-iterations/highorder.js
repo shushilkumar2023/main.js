0f35069e55917263979b7b590e799f995f730384
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-24 07:16:56 +0000

    highorder

---
+++ 05-iterations/highorder.js
@@ -0,0 +1,9 @@
+// const myArray = [1,2,3,4,7,]
+// for (const num of myArray) {
+//     console.log(num);
+// }
+
+const str = "shuhsil"
+for (const string of str) {
+    console.log(`value of ${str}`);
+}
