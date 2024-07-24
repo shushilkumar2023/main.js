8a142ba90181a452e1cc94a949baae1fdd166ac9
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-24 06:54:53 +0000

    whileloop

---
+++ 05-iterations/whileanddowhileloop.js
@@ -0,0 +1,12 @@
+// const myArray = ["spiderman","flash","batman"]
+// let arr = 0
+// while (arr <= myArray.length) {
+//     console.log(`print myarray ${myArray}`);
+//     arr = arr + 1
+// }
+
+// let score = 1
+// do {
+//     console.log(`my score is ${score}`);
+//     score++
+// } while (score <= 10);
