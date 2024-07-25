da6cae80ec1dc6c095764a62a842fc0e421360f4
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-25 06:56:45 +0000

    highorder

--- 05-iterations/highorder.js
+++ 05-iterations/highorder.js
@@ -1,3 +1,4 @@
+// for of loop use Array
 // const myArray = [1,2,3,4,7,]
 // for (const num of myArray) {
 //     console.log(num);
@@ -10,9 +11,28 @@
 
 // maps 
 
-const map = new Map ()
-map.set('In', "India")
-map.set('USA', "United stste of America")
-map.set('Fr', "france")
+// const map = new Map ()
+// map.set('In', "India")
+// map.set('USA', "United stste of America")
+// map.set('Fr', "france")
 
-console.log(map);
+// console.log(map);
+ // for in loop use object
+const myObject = {
+    js: "Javascript",
+    cpp: "c++",
+    php: "python",
+    sky: "shuhsil kumar yadav"
+}
+// 
+
+for (const key in myObject) {
+ //  console.log(`${key} and ${myObject[key]}`);
+}
+
+const programming = ["js", "php", "html", "css"]
+
+for (const key in programming) {
+ console.log(`${programming[key]}`); 
+
+}
