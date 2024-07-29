265a598c0f9e880b1d48fbe7e2745fa6ce09b017
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-29 06:46:24 +0000

    six

--- 05-iterations/six.js
+++ 05-iterations/six.js
@@ -1,20 +1,28 @@
-const codename =["js","cpp","java"]
-const values = codename.forEach( (item) =>{
-    console.log(item);
-    return item
-})
+// const codename =["js","cpp","java"]
+// const values = codename.forEach( (item) =>{
+//     console.log(item);
+//     return item
+// })
 
-console.log(values);
+// console.log(values);
 
-const myNums = [1,2,3,4,5,6,7,8,9,10]
- const newnums = myNums.filter( (num) => {
-    return num > 4
- })
- console.log(newnums);
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+//  const newnums = myNums.filter( (num) => {
+//     return num > 4
+//  })
+//  console.log(newnums);
 
- const newNums = []
- newNums.forEach( (num) => {
-    if (num > 4) {
-        numNums.push(num)
-    }
- })
+//  const newNums = []
+//  newNums.forEach( (num) => {
+//     if (num > 4) {
+//         numNums.push(num)
+// //     }
+// //  })
+
+// const mynums = [1,2,3,4,5,6,7,8,9,10]
+// // const newNums = mynums.map((num)=> { num + 10})
+// // console.log(newNums);
+
+// const newNums = mynums.map( (num) => num*10).map( (num) => num+1).filter( (num) => num >= 41)
+
+// console.log(newNums);
