39ca40f2e3dcf2a68e0f4c630082ce596c3f6268
shushilkumar2023 <138500032+shushilkumar2023@users.noreply.github.com>
2024-07-29 07:40:35 +0000

    seven

---
+++ 05-iterations/seven.js
@@ -0,0 +1,42 @@
+// // const array1 = [1,2,3,4,5]
+// // const initialValue = 0;
+// // const sumWithInitial = array1.reduce(
+// // (accumlator, currentvalue) => accumlator + currentvalue,initialValue
+// // // )
+// // // console.log(sumWithInitial);
+
+// // const num = [1,,5,4,7]
+
+// // const total = num.reduce(function (acc, currval) {
+// //     console.log(`acc${acc} and currval${currval}`);
+// //    return  acc + currval
+// // }, 0)
+
+// // console.log(total);
+
+// const shoppinfCart = [
+//     {
+//         course: "js course",
+//         price: 2542
+//     },
+//     {
+//         course: "Python course",
+//         price: 3999
+//     },
+//     {
+//         course: "cpp course",
+//         price: 4399
+//     },
+//     {
+//         course: "data science  course",
+//         price: 2992
+//     },
+//     {
+//         course: "java course",
+//         price: 2471
+//     },
+// ]
+
+// const shoppingTopay = shoppinfCart.reduce((acc , item) => acc + item.price, 0)
+
+//  console.log(shoppingTopay);
